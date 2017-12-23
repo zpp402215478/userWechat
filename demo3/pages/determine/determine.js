400bfc58eceff41e5da5d212f132775c38178502
zppcc0410 <zpp402215478@163.com>
2017-12-23 15:32:23 +0800

    aa

--- demo3/pages/determine/determine.js
+++ demo3/pages/determine/determine.js
@@ -1,18 +1,61 @@
 var app = getApp()
 Page({
   data: {
-   
+   checkFood :[],
+   checkPriceAndName:[],
+   checkMoney : 0,
+   deliverprice : 0
   },
   onLoad: function (options) {
     var that = this
     var id = options.id;
-    console.log(id)
     wx.getStorage({
       key: 'cargo',
       success: function (res) {
-        console.log(res.data)
+        that.setData({
+        checkFood: res.data.list,
+        checkPriceAndName: res.data.priceAndName,
+        checkMoney: res.data.total         
+        })
       }
     })
+
+    wx.request({
+      url: getApp().data.servsers + '/fastShopAction/getFashShopById/' + id + '.do',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        that.setData({
+          checkMoney: that.data.checkMoney + res.data.deliverprice,
+          deliverprice: res.data.deliverprice
+        })
+      }
+    })
+  },
+
+  submit: function(e){
+    if (e.detail.value.username.length == 0) {
+      wx.showToast({
+        title: '昵称不得为空!',
+        icon: 'loading',
+        duration: 1500
+      })
+    }
+    if ( e.detail.value.telephone.length == 0) {
+      wx.showToast({
+        title: '手机号码不得为空!',
+        icon: 'loading',
+        duration: 1500
+      })
+    }
+    if (e.detail.value.address.length == 0) {
+      wx.showToast({
+        title: '送餐地址不得为空!',
+        icon: 'loading',
+        duration: 1500
+      })
+    }
   },
 
   onReady: function () {
